ec1ae05faeb1edafb38eda13dec544ec23bdfe83
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-06-17 14:13:16 +0700

    update

--- resources/views/pages/porto/blog.blade.php
+++ resources/views/pages/porto/blog.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Projects | Porotofolio
+    Blog | Porotofolio
 @endsection
 
 
@@ -14,31 +14,20 @@
                 
                 <div class="col-lg-7 col-md-12 col-12 d-flex align-items-center">
                     <div class="about-text">
-                        <small class="small-text">Welcome to <span class="mobile-block">my portfolio website!</span></small>
                         <h1 class="animated animated-text">
-                            <span class="mr-2">Hey Everyone, I'm</span>
+                            <span class="mr-2">Blog</span>
                                 <div class="animated-info">
-                                    <span class="animated-item">Marvel Sann</span>
-                                    <span class="animated-item">Web Designer</span>
-                                    <span class="animated-item">UI Specialist</span>
+                                    <span class="animated-item">Coming Soon</span>
+                                    <span class="animated-item">Coming Soon</span>
+                                    <span class="animated-item">Coming Soon</span>
                                 </div>
                         </h1>
 
-                        <p>Building a successful product is a challenge. I am highly energetic in user experience design, interfaces and web development.</p>
+                        <p>Stay tune</p>
                         
-                        <div class="custom-btn-group mt-4">
-                          <a href="#" class="btn mr-lg-2 custom-btn"><i class='uil uil-file-alt'></i> Download Resume</a>
-                          <a href="#contact" class="btn custom-btn custom-btn-bg custom-btn-link">Get a free quote</a>
-                        </div>
+                        
                     </div>
                 </div>
-
-                <div class="col-lg-5 col-md-12 col-12">
-                    <div class="about-image svg">
-                        <img src="images/undraw/logo.svg" class="img-fluid" alt="svg image">
-                    </div>
-                </div>
-
             </div>
         </div>
     </section>
